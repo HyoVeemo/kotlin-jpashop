64242820230d7aa77a857a8bb0da0f062e96fcb1
hyoseon <hyoseon.jegal@frientrip.com>
2024-01-20 13:53:46 +0900

    faet: 상품 엔티티 비즈니스 로직 추가

--- src/main/kotlin/jpabook/jpashop/domain/Item.kt
+++ src/main/kotlin/jpabook/jpashop/domain/Item.kt
@@ -1,22 +1,43 @@
 package jpabook.jpashop.domain
 
 import jakarta.persistence.*
+import jpabook.jpashop.exception.NotEnoughStockException
 
 @Entity
-@Inheritance (strategy = InheritanceType.SINGLE_TABLE)
-abstract class Item (
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+abstract class Item(
     @Id
     @GeneratedValue
-    @Column(name="item_id")
+    @Column(name = "item_id")
     val id: Long,
 
     val name: String,
 
-    val price: Int,
+    val price: Int = 0,
 
-    val stockQuantity: Int,
-
-    ){
+    var stockQuantity: Int = 0,
+) {
     @ManyToMany
     val categories: List<Category> = emptyList()
+
+    // -- 비즈니스 로직 -- //
+
+    /**
+     * stock 증가
+     */
+    fun addStock(quantity: Int) {
+        this.stockQuantity += quantity
+    }
+
+    /**
+     * stock 감소
+     */
+    fun removeStock(quantity: Int) {
+        if (this.stockQuantity - quantity < 0) {
+            throw NotEnoughStockException("need more stock")
+        }
+        
+        this.stockQuantity -= quantity
+    }
+
 }
