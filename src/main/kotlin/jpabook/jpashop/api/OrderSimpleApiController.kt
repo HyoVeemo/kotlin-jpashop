865fd1c6cbaa6dbac2e44799addbd20a6f071555
hyoseon <hyoseon.jegal@frientrip.com>
2024-01-27 19:41:20 +0900

    feat: 간단한 주문 조회 v2: 엔티티 DTO 로 변환

--- src/main/kotlin/jpabook/jpashop/api/OrderSimpleApiController.kt
+++ src/main/kotlin/jpabook/jpashop/api/OrderSimpleApiController.kt
@@ -1,10 +1,13 @@
 package jpabook.jpashop.api
 
+import jpabook.jpashop.domain.Address
 import jpabook.jpashop.domain.Order
+import jpabook.jpashop.domain.OrderStatus
 import jpabook.jpashop.repository.OrderRepository
 import jpabook.jpashop.repository.OrderSearch
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
 
 
 /**
@@ -29,4 +32,26 @@ class OrderSimpleApiController(
 
         return all
     }
+
+
+    @GetMapping("/api/v2/simple-orders")
+    fun ordersV2(): List<SimpleOrderDto> {
+        val orders = orderRepository.findAll(OrderSearch())
+        return orders.map {
+            SimpleOrderDto(it)
+        }
+    }
+
+    companion object {
+        data class SimpleOrderDto(
+            private val order: Order
+        ) {
+            val orderId: Long = order.id;
+            val name: String = order.member.name;
+            val orderDate: LocalDateTime = order.orderDate;
+            val orderStatus: OrderStatus = order.status;
+            val address: Address = order.delivery.address;
+        }
+    }
 }
+
