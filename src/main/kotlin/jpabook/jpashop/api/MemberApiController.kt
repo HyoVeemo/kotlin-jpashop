7a22cb3f21ff3dd6bf3f0aa6d73706a4b0891b8b
hyoseon <hyoseon.jegal@frientrip.com>
2024-01-27 17:33:24 +0900

    feat: 회원 목록 Api

--- src/main/kotlin/jpabook/jpashop/api/MemberApiController.kt
+++ src/main/kotlin/jpabook/jpashop/api/MemberApiController.kt
@@ -12,6 +12,19 @@ class MemberApiController(
     private val memberService: MemberService
 ) {
 
+    @GetMapping("/api/v1/members")
+    fun membersV1(): List<Member> {
+        return memberService.findMembers()
+    }
+
+    @GetMapping("/api/v2/members")
+    fun membersV2(): Result<List<MemberDto>> {
+        val findMembers = memberService.findMembers()
+        val collect = findMembers.map { MemberDto(it.name) }
+            .toList()
+        return Result(data = collect, count = collect.size)
+    }
+
     @PostMapping("/api/v1/members")
     fun saveMemberV1(@RequestBody @Valid member: Member): CreateMemberResponse {
         val id = memberService.join(member)
@@ -35,27 +48,42 @@ class MemberApiController(
         return UpdateMemberResponse(member.id, member.name)
     }
 
-    @Data
-    data class CreateMemberResponse(
-        val id: Long
-    )
+    companion object {
+        @Data
+        data class Result<T>(
+            val data: T,
+            val count: Int
+        )
 
-    @Data
-    data class CreateMemberRequest(
-        @field:NotEmpty
-        val name: String
-    )
+        @Data
+        data class MemberDto(
+            val name: String
+        )
 
-    @Data
-    data class UpdateMemberResponse(
-        val id: Long,
-        val name: String
-    )
+        @Data
+        data class CreateMemberResponse(
+            val id: Long
+        )
 
-    @Data
-    data class UpdateMemberRequest(
-        val name: String
-    )
+        @Data
+        data class CreateMemberRequest(
+            @field:NotEmpty
+            val name: String
+        )
+
+        @Data
+        data class UpdateMemberResponse(
+            val id: Long,
+            val name: String
+        )
+
+        @Data
+        data class UpdateMemberRequest(
+            val name: String
+        )
+    }
+
+
 }
 
 
